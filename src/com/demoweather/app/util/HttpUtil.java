c42413a6946c86db5330a81781232c20fa6f307e
LuckyJ <lzhhj@live.com>
2016-08-01 11:04:33 +0800

    delete gen dir

---
+++ src/com/demoweather/app/util/HttpUtil.java
@@ -0,0 +1,91 @@
+package com.demoweather.app.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.demoweather.app.activity.MyApplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class HttpUtil {
+    public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
+        if (!isNetworkAvailable(MyApplication.getContext())) {
+            // Toast.makeText(MyApplication.getContext(), "network is
+            // unavailable", Toast.LENGTH_SHORT).show();
+            if (listener != null) {
+                // 回调onError()方法
+                listener.onError(new RuntimeException("network is unavailable"));
+            }
+            return;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    if (listener != null) {
+                        // 回调onFinish()方法
+                        listener.onFinish(response.toString());
+                    }
+                } catch (Exception e) {
+                    if (listener != null) {
+                        // 回调onError()方法
+                        listener.onError(e);
+                    }
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        if (context.checkCallingOrSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            ConnectivityManager connectivity = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            if (connectivity == null) {
+                Log.w("HttpUtil", "--->>couldn't get connectivity manager");
+            } else {
+                NetworkInfo[] info = connectivity.getAllNetworkInfo();
+                if (info != null) {
+                    for (int i = 0; i < info.length; i++) {
+                        if (info[i].isAvailable()) {
+                            Log.d("HttpUtil", "--->>network is available");
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        Log.d("HttpUtil", "--->>network is not available");
+        return false;
+    }
+}
